29a10009e5d5800db59b0e0bbe7bc81c5823d9b8
osantamaria <osantamaria@gmail.com>
2025-03-13 09:31:09 -0600

    refactoring project setup

--- dating-platform-for-dogs-master/src/main/java/com/dbapresents/datingplatform/profile/ProfileRepository.java
+++
@@ -1,6 +0,0 @@
-package com.dbapresents.datingplatform.profile;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
-}
---
+++ dogdating/src/main/java/com/ossant/dogdating/DogdatingApplication.java
@@ -0,0 +1,13 @@
+package com.ossant.dogdating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogdatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogdatingApplication.class, args);
+	}
+
+}
